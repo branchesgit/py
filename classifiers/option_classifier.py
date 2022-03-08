a96b419e10369c48f993ba10d2e5b103d401f55c
branches <3085038458@qq.com>
2022-03-09 01:07:54 +0800

    change logic

--- classifiers/option_classifier.py
+++ classifiers/option_classifier.py
@@ -1,12 +1,28 @@
 
-from project import vProject
+from fileinput import close
+from math import ceil
+from project import get_target_concentrated, vProject
 import cv2
 from rect import Rect, sort_y, sort_x
+import numpy as np
 class OptionClassifier():
     def set_direction(self, direction):
         self.direction = direction
 
     def classifier(self, mat):
+        ret,thresh = cv2.threshold(mat,127,255,0)
+        contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        areas = []
+        height, width = mat.shape
+        m = 1000
+        for i in range(0,len(contours)):  
+            x, y, w, h = cv2.boundingRect(contours[i])   
+            areas.append(w * h)
+            m = min(m, w * h)
+            if w < 3 * width / 4 and h < 3 * height / 4:
+                if w * h >= 10:
+                    cv2.rectangle(mat, (x,y), (x+w,y+h), (0,0,0), -1) 
+
         if self.direction == "horition":
             print('horition')
         else: 
@@ -32,8 +48,27 @@ class OptionClassifier():
             if i >= 1:
                 gaps.append(rect.x - x_rects[i -1].x - x_rects[i-1].w)
 
-        print(gaps, 'gaps', ws)
-
+        
+        c_gaps = [i for i in gaps]
+        gaps.sort()
+        gaps_len = len(gaps) - 1
+        m = gaps[gaps_len]
+        count = 0
+        middle = gaps[ceil(gaps_len / 2)]
+        value = ceil((middle + m) / 2)
+        while gaps_len >= 0:
+            gaps_len -= 1
+            if gaps[gaps_len] >= value:
+                count += 1
+        
+        n = gaps[gaps_len - count + 1]
+        
+        indexs = []
+        for i in range(len(c_gaps)):
+            if c_gaps[i] >= n and c_gaps[i] <= m:
+                indexs.append(i)
+        print(indexs, c_gaps)
+        
 
     # 得到最小的矩形框
     def get_min_rect(self, mat, rect):
